728b87541fe6241b094f4dcd9cd17f396d0fce86
webbchrisuk-max <webbchris.uk@icloud.com>
2026-09-01 09:42:26 +0100

    Remove legacy root income dividend countdown patch

--- income-next-dividend-countdown.js
+++
@@ -1,5 +0,0 @@
-(() => {
-  'use strict';
-  // Retired: next-dividend date/countdown is now rendered directly by income-restored.js.
-  window.__AuroraIncomeNextDividendCountdown = 'RETIRED_MAIN_RENDERER_OWNS_HERO';
-})();
